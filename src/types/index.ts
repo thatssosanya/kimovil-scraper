f5b2e294dbd05860a1de5b953cfa67410264127d
thatssosanya <feelsknight@gmail.com>
2025-01-10 00:18:24 +0500

    feat: add new fields

--- src/types/index.ts
+++ src/types/index.ts
@@ -9,7 +9,11 @@ export type Sim = (typeof SIM_TYPES)[number];
 
 export type DirtySku = { ram: number; rom: number };
 export type DirtyMarket = { mkid: string; devices: DirtySku[] };
-export type Sku = { marketId: string; ram_gb: number; storage_gb: number };
+export type Sku = {
+  marketId: string; // |-delimited
+  ram_gb: number;
+  storage_gb: number;
+};
 
 export interface SingleCameraData {
   resolution_mp: number;
@@ -17,6 +21,11 @@ export interface SingleCameraData {
   sensor: string | null;
   type: string;
   features: string;
+}
+
+export interface Benchmark {
+  name: string;
+  score: number;
 }
 
 // fields representing string[] are cast to |-delimited strings
@@ -43,29 +52,40 @@ export interface PhoneData {
   size_in: number | null;
   displayType: string | null;
   resolution: string | null;
+  aspectRatio: string | null;
   ppi: number | null;
   displayFeatures: string; // |-delimited
 
   // hardware
   cpu: string | null;
+  cpuManufacturer: string | null;
+  cpuCores: string | null; // |-delimited
   gpu: string | null;
   sdSlot: boolean | null;
   skus: Sku[];
+  fingerprintPosition: string | null;
+  benchmarks: Benchmark[];
 
   // connectivity
   nfc: boolean | null;
   bluetooth: string | null;
   sim: string; // |-delimited
-  usb: "USB-C" | "Lightning" | "MicroUSB" | null;
+  simCount: number;
+  usb: "USB-C" | "Lightning" | null;
   headphoneJack: boolean | null;
 
   // battery
   batteryCapacity_mah: number | null;
   batteryFastCharging: boolean | null;
+  batteryWattage: number | null;
 
   // cameras
   cameras: SingleCameraData[];
   cameraFeatures: string; // |-delimited
+
+  // software
+  os: string | null;
+  osSkin: string | null;
 }
 
 export { SIM_TYPES as simTypes };
